37e9e2a579a970bd44f8260b17d909bb98d9d5d5
Niall Lewin <Lewin91@hotmail.com>
2018-03-27 12:59:27 +0100

    Updated "ItemManager.cs"
    Added the "_resources" field.
    Added the constructor.
    Added the "AddToQueue" method.
    Added the "Update" method.

--- VillageBackend/Managers/ItemManager.cs
+++ VillageBackend/Managers/ItemManager.cs
@@ -11,6 +11,11 @@ namespace VillageBackend.Managers
   public class ItemManager
   {
     /// <summary>
+    /// The resources from the actual game
+    /// </summary>
+    private Resources _resources;
+    
+    /// <summary>
     /// All items that have been crafted
     /// </summary>
     public List<ItemV2> Items { get; private set; }
@@ -18,6 +23,38 @@ namespace VillageBackend.Managers
     /// <summary>
     /// The items added to the queue to be crafed by the assigned villager
     /// </summary>
-    public List<ItemV2> QueuedItems { get; private set; }    
+    public List<ItemV2> QueuedItems { get; private set; }
+		
+		public ItemManager(Resources resources)
+		{
+			_resources = resources;
+		}
+    
+    public void AddToQueue(ItemV2 item)
+    {
+      if(!Resources.CanAfford(_resources, item.ResourceCost)
+        throw new Exception("Check to see if the item is affordable before adding to q");
+      
+			QueuedItems.Add(item);
+    }
+		
+		public void Update(GameTime gameTime)
+		{
+			foreach (var items in QueuedItems.GroupBy(c => c.CrafterId))
+			{
+				var firstItem = items.FirstOrDefault();
+				
+				if (firstItem != null)
+				{
+					firstItem.CraftingTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+					
+					if (firstItem.CraftingTime >= firstItem.CraftTime)
+					{
+						Items.Add(firstItem);
+						QueuedItems.Remove(firstItem);
+					}
+				}
+			}
+		}
   }
 }
