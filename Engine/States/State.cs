38fa1270622288f0c782c6178de9f9c5546a1e33
Niall Lewin <Lewin91@hotmail.com>
2018-04-05 12:00:55 +0100

    Added Window cache
    Created a cache of "_windows" to store the state of the window when it's closed.

--- Engine/States/State.cs
+++ Engine/States/State.cs
@@ -44,6 +44,8 @@ namespace Engine.States
 
     protected Window _window;
 
+    private List<Window> _windows;
+
     public bool IsWindowOpen
     {
       get
@@ -71,6 +73,22 @@ namespace Engine.States
 
     public void CloseWindow()
     {
+      if (_window == null)
+        return;
+
+      if (_windows.Any(c => c.GetType() == _window.GetType()))
+      {
+        for (int i = 0; i < _windows.Count; i++)
+        {
+          if (_windows[i].GetType() == _window.GetType())
+            _windows[i] = _window;
+        }
+      }
+      else
+      {
+        _windows.Add(_window.Clone() as Window);
+      }
+
       _window = null;
     }
 
@@ -79,13 +97,30 @@ namespace Engine.States
     public virtual void LoadContent(GameModel gameModel)
     {
       _gameModel = gameModel;
+
+      _windows = new List<Window>();
     }
 
     public abstract void OnScreenResize();
 
     public void OpenWindow(Window window)
     {
-      _window = window;
+      if (_windows.Any(c => c.GetType() == window.GetType()))
+      {
+        for (int i = 0; i < _windows.Count; i++)
+        {
+          if (_windows[i].GetType() == window.GetType())
+          {
+            _window = _windows[i];
+            // TODO: Make sure the content of the selected category is updated
+            return;
+          }
+        }
+      }
+      else
+      {
+        _window = window;
+      }
     }
 
     public abstract void PostUpdate(GameTime gameTime);
